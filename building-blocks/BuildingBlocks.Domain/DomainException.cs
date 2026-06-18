e3258be2320b5ac2bb5b922414f3bf40ae4463de
Shehroz <shehroz.amjad@arabsea.com>
2026-06-18 17:23:39 +0500

    domain common

---
+++ building-blocks/BuildingBlocks.Domain/DomainException.cs
@@ -0,0 +1,18 @@
+﻿namespace BuildingBlocks.Domain;
+
+public class DomainException : Exception
+{
+    public string ErrorCode { get; }
+
+    public DomainException(string message, string errorCode = "DOMAIN_ERROR")
+        : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+
+    public DomainException(string message, Exception innerException, string errorCode = "DOMAIN_ERROR")
+        : base(message, innerException)
+    {
+        ErrorCode = errorCode;
+    }
+}
---
+++ building-blocks/BuildingBlocks.Domain/IAggregateRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Domain;
+
+public interface IAggregateRoot
+{
+    IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
+    void ClearDomainEvents();
+}
